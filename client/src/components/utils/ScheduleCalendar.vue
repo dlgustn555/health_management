c3465e459b3f0b659c82a57b998ee163be09d4f4
hyunsu.lee <hyunsu.lee@navercorp.com>
2019-02-18 09:41:04 +0900

    [Cleint] PC > 스캐줄 캘린더 컨포넌트 구현 https://github.com/dlgustn555/health/issues/12

--- client/src/components/utils/ScheduleCalendar.vue
+++ client/src/components/utils/ScheduleCalendar.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="schedulecalendar_container">
     <div class="selected_month">
-      <button @click="setSchedules(STATUS.PREVE)">이전</button>
-      <button @click="setSchedules(STATUS.NOW)">오늘</button>
-      <button @click="setSchedules(STATUS.NEXT)">다음</button>
+      <button @click="changeYear(STATUS.PREV)">이전년도</button>
+      <button @click="changeMonth(STATUS.PREV)">이전</button>
+      <button @click="changeMonth(STATUS.RESET)">오늘</button>
+      <button @click="changeMonth(STATUS.NEXT)">다음</button>
+      <button @click="changeYear(STATUS.NEXT)">다음년도</button>
       <select v-model="START_DAY">
         <option value="SUNDAY">일요일</option>
         <option value="MONDAY">월요일</option>
       </select>
-      <h1 class="date">{{ oDate.getFullYear() }}.{{ oDate.getMonth() + 1 }}월</h1>
+      <input
+        id="check_full"
+        v-model="IS_FULL"
+        type="checkbox">
+      <label for="check_full">FULL</label>
+      <h1 class="date">{{ year }}.{{ month + 1 }}월</h1>
     </div>
     <div>
       <div class="week header">
@@ -25,17 +32,20 @@
         :key="weekIndex"
         class="week">
         <div
-          v-for="(schedule, colIndex) in getWeekSchedules(weekIndex)"
+          v-for="(schedule, colIndex) in getWeekSchedules(weekIndex, STATUS.NOW)"
           :key="colIndex"
           :class="['cell', schedule.isToday ? 'today' : '']">
-          <div v-if="schedule.isDisplay">{{ schedule.date }}</div>
-          <div>{{ schedule.data }}</div>
+          <div v-if="schedule.isDisplay">
+            <div :class="schedule.status">{{ schedule.date }}</div>
+            <div>{{ schedule.data }}</div>
+          </div>
         </div>
       </div>
     </div>
-    <pre>
-      {{ result }}
-    </pre>
+    <p>{{ test }}</p>
+    <p>{{ test }}</p>
+    <p>{{ test }}</p>
+    <p>{{ test }}</p>
   </div>
 </template>
 <script>
@@ -44,11 +54,12 @@ export default {
   data() {
     return {
       STATUS: {
-        PREVE: 'preve',
+        PREV: 'prev',
         NOW: 'now',
-        NEXT: 'next'
+        NEXT: 'next',
+        RESET: 'reset'
       },
-      MAX_ROWS: 5,
+      IS_FULL: false,
       MAX_COLS: 7,
       START_DAY: 'SUNDAY',
       DAYS: {
@@ -56,55 +67,117 @@ export default {
         SUNDAY: ['일', '월', '화', '수', '목', '금', '토']
       },
       MONTH: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-      oDate: null,
-      schedules: [],
+      oDate: new Date(),
       result: ''
     }
   },
   computed: {
+    test() {
+      console.log('TEST')
+      return 'test'
+    },
+    schedules() {
+      return {
+        prev: this._getSchedules(this.STATUS.PREV),
+        now: this._getSchedules(this.STATUS.NOW),
+        next: this._getSchedules(this.STATUS.NEXT)
+      }
+    },
     DAY_PER_MONTHS() {
-      const year = this.oDate.getFullYear
+      const year = this.oDate.getFullYear()
       const twoMonthDay =
         (year % 100 !== 0 && year % 4 === 0) || year % 400 === 0 ? 29 : 28
       return [31, twoMonthDay, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    },
+    MAX_ROWS() {
+      // const startDays = this.schedules.now.filter(({ isDisplay, index }) => {
+      //   return isDisplay && (index === 0 || index % 7 === 0)
+      // })
+      const startDays = [1, 2, 3, 4, 5]
+      return startDays.length
+    },
+    year() {
+      return this.oDate.getFullYear()
+    },
+    month() {
+      return this.oDate.getMonth()
+    },
+    todate() {
+      const oTodate = new Date()
+      return `${oTodate.getFullYear()}${oTodate.getMonth()}${oTodate.getDate()}`
     }
   },
   async created() {
-    this.setSchedules(this.STATUS.NOW)
-    const url = '/health'
-    const { status, data } = await this.$axios.get(url)
-    if (status === 200 && data.success) {
-      this.result = data.message
-    }
+    // const url = '/health'
+    // const { status, data } = await this.$axios.get(url)
+    // if (status === 200 && data.success) {
+    //   this.result = data.message
+    // }
   },
   methods: {
-    _getOption(status) {
-      const year = this.oDate.getFullYear()
-      const month = this.oDate.getMonth()
+    changeYear(status) {
+      const { STATUS, oDate, year, month } = this
+      const changeYear = status === STATUS.PREV ? year - 1 : year + 1
+      this.oDate = new Date(changeYear, month)
+    },
+    changeMonth(status) {
+      this.oDate = this._getDateByMonth(status)
+    },
+    getWeekSchedules(weekIndex, status) {
+      const { schedules, IS_FULL, MAX_ROWS, MAX_COLS, STATUS } = this
+      const begin = weekIndex * MAX_COLS
+      const end = begin + MAX_COLS
+      const weekSchedules = schedules[status].slice(begin, end)
+
+      if (!IS_FULL) {
+        return weekSchedules
+      }
+
+      if (status === STATUS.NOW && weekIndex === 0) {
+        const pwIndex = MAX_ROWS - 1
+        const pwSchedules = this.getWeekSchedules(pwIndex, STATUS.PREV)
+        const startDay = this._getStartDay(this.year, this.month)
+        return [
+          ...pwSchedules.slice(0, startDay),
+          ...weekSchedules.slice(startDay)
+        ]
+      }
+
+      if (status === this.STATUS.NOW && weekIndex === MAX_ROWS - 1) {
+        const lastDay = this._getLastDay(this.year, this.month) + 1
+        const nextWekkSchedules = this.getWeekSchedules(0, STATUS.NEXT)
+        return [
+          ...weekSchedules.splice(0, lastDay),
+          ...nextWekkSchedules.splice(lastDay)
+        ]
+      }
+      return weekSchedules
+    },
+    _getSchedules(status) {
+      const oDate = this._getDateByMonth(status)
+      const oDateOtion = this._getDateOption(oDate, status)
+      const { MAX_ROWS, MAX_COLS } = this
+      return Array.apply(null, Array(MAX_ROWS * MAX_COLS)).map(
+        (empty, index) => {
+          return {
+            index,
+            status,
+            ...this._getColumnDate(oDateOtion, index)
+          }
+        }
+      )
+    },
+    _getDateOption(oDate, status) {
+      const year = oDate.getFullYear()
+      const month = oDate.getMonth()
       return {
         status,
         year,
         month,
         startDay: this._getStartDay(year, month),
-        lastDate: this.DAY_PER_MONTHS[month],
-        todate: this.oDate.getDate()
+        lastDate: this.DAY_PER_MONTHS[month]
       }
     },
-    setSchedules(status) {
-      this.oDate = this._getDateByMonth(status)
-      const oDateOtion = this._getOption(status)
-      const { MAX_ROWS, MAX_COLS } = this
-      this.schedules = Array.apply(null, Array(MAX_ROWS * MAX_COLS)).map(
-        (empty, index) => {
-          return {
-            index,
-            ...this._getColumnDate(oDateOtion, index),
-            data: {}
-          }
-        }
-      )
-    },
-    // 시작요일
     _getStartDay(year, month) {
       const SUNDAY = 0
       const date = 1
@@ -114,23 +187,34 @@ export default {
       }
       return startDay
     },
+    _getLastDay(year, month) {
+      const SUNDAY = 0
+      const date = this.DAY_PER_MONTHS[month]
+      let lastDay = new Date(year, month, date).getDay()
+      if (this.START_DAY === 'MONDAY') {
+        lastDay = lastDay === SUNDAY ? 0 : lastDay - 1
+      }
+      return lastDay
+    },
     _getDateByMonth(status) {
-      const { STATUS, oDate } = this
-      if (status === STATUS.PREVE) {
-        return new Date(oDate.getFullYear(), oDate.getMonth() - 1)
+      const { STATUS, oDate, year, month } = this
+      if (status === STATUS.PREV) {
+        return new Date(year, month - 1)
       } else if (status === STATUS.NEXT) {
-        return new Date(oDate.getFullYear(), oDate.getMonth() + 1)
-      } else {
+        return new Date(year, month + 1)
+      } else if (status === STATUS.RESET) {
         return new Date()
+      } else {
+        return oDate
       }
     },
     _getColumnDate(oDateOtion, index) {
-      const { status, year, month, startDay, lastDate, todate } = oDateOtion
+      const { status, year, month, startDay, lastDate } = oDateOtion
       const oColumnDate = {
-        isDisplay: false,
         year,
         month: month + 1,
-        date: 0
+        date: 0,
+        isDisplay: false
       }
 
       if (index === 0 && index === startDay) {
@@ -140,19 +224,13 @@ export default {
         const date = index - startDay + 1
         oColumnDate.isDisplay = true
         oColumnDate.date = date
-        oColumnDate.isToday = status === this.STATUS.NOW && date === todate
+        oColumnDate.isToday = `${year}${month}${date}` === `${this.todate}`
       }
 
       if (oColumnDate.date > lastDate) {
         oColumnDate.isDisplay = false
       }
       return oColumnDate
-    },
-    getWeekSchedules(weekIndex) {
-      const { schedules, MAX_COLS } = this
-      const begin = weekIndex * MAX_COLS
-      const end = begin + MAX_COLS
-      return schedules.slice(begin, end)
     }
   }
 }
@@ -172,5 +250,9 @@ export default {
 }
 .cell.today {
   background: sandybrown;
+}
+.cell .prev,
+.cell .next {
+  font-style: italic;
 }
 </style>
